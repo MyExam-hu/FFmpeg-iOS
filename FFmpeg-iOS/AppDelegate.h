42bde5486490b3700e2e3d2af50fd1fa7d1f4dc4
huweidong <407475190@qq.com>
2016-12-27 11:45:47 +0800

    Initial Commit

---
+++ FFmpeg-iOS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FFmpeg-iOS
+//
+//  Created by huweidong on 27/12/16.
+//  Copyright © 2016年 huweidong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FFmpeg-iOS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FFmpeg-iOS
+//
+//  Created by huweidong on 27/12/16.
+//  Copyright © 2016年 huweidong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
